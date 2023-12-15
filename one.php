38424e11da51af9129d1a1f36444cc5c8dc8dcbb
Reza Hadiwijaya Dynasti <reza@rehad.id>
2023-12-15 10:26:05 +0800

    first commit

---
+++ one.php
@@ -0,0 +1,86 @@
+<section id="one" class="wrapper style3 fullscreen fade-up">
+  <div class="inner">
+    <h2>About Me</h2>
+    <p>
+      Hello, my name is Reza Hadiwijaya Dynasti, S.Pd. I'm currently completing my Master's degree in Special
+      Education and will graduate in 2023. I have 3 years of work experience in the field of education and online
+      classroom administration. I have skills in planning and teaching educational programs, as well as effectively
+      managing both online and offline classes. Additionally, I'm actively involved in several organizations, both as a
+      member and as an administrator.
+      <br>
+      <br>
+      I have experience as a staff admin in the LMS department at Universitas Negeri Makassar and as an exam monitor at
+      the Ministry of Education, Culture, Research, and Technology (KEMENDIKBUDRISTEK). I possess skills in educational
+      media development, information technology, and analysis, and I'm capable of providing appropriate education to
+      children with disabilities and college students specializing in special education. I'm enthusiastic about sharing
+      my experiences and knowledge with others, and I hope to continue growing and learning in the field of education.
+      Thank you for visiting my profile.
+    </p>
+    <h3>Social</h3>
+    <ul class="icons">
+      <li>
+        <a href="https://github.com/rehadyn" class="icon brands fa-github fa-2x
+"><span class="label">GitHub</span></a>
+      </li>
+      <li>
+        <a href="https://www.instagram.com/rehad_id" class="icon brands fa-instagram fa-2x
+"><span class="label">Instagram</span></a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/rehad-id/" class="icon brands fa-linkedin-in fa-2x
+"><span class="label">LinkedIn</span></a>
+      </li>
+    </ul>
+    <div class="features">
+
+      <!-- <section>
+              <span class="icon solid major fa-code"></span>
+              <h3>Lorem ipsum amet</h3>
+              <p>
+                Phasellus convallis elit id ullam corper amet et pulvinar. Duis
+                aliquam turpis mauris, sed ultricies erat dapibus.
+              </p>
+            </section>
+            <section>
+              <span class="icon solid major fa-lock"></span>
+              <h3>Aliquam sed nullam</h3>
+              <p>
+                Phasellus convallis elit id ullam corper amet et pulvinar. Duis
+                aliquam turpis mauris, sed ultricies erat dapibus.
+              </p>
+            </section>
+            <section>
+              <span class="icon solid major fa-cog"></span>
+              <h3>Sed erat ullam corper</h3>
+              <p>
+                Phasellus convallis elit id ullam corper amet et pulvinar. Duis
+                aliquam turpis mauris, sed ultricies erat dapibus.
+              </p>
+            </section>
+            <section>
+              <span class="icon solid major fa-desktop"></span>
+              <h3>Veroeros quis lorem</h3>
+              <p>
+                Phasellus convallis elit id ullam corper amet et pulvinar. Duis
+                aliquam turpis mauris, sed ultricies erat dapibus.
+              </p>
+            </section>
+            <section>
+              <span class="icon solid major fa-link"></span>
+              <h3>Urna quis bibendum</h3>
+              <p>
+                Phasellus convallis elit id ullam corper amet et pulvinar. Duis
+                aliquam turpis mauris, sed ultricies erat dapibus.
+              </p>
+            </section>
+            <section>
+              <span class="icon major fa-gem"></span>
+              <h3>Aliquam urna dapibus</h3>
+              <p>
+                Phasellus convallis elit id ullam corper amet et pulvinar. Duis
+                aliquam turpis mauris, sed ultricies erat dapibus.
+              </p>
+            </section> -->
+    </div>
+  </div>
+</section>
